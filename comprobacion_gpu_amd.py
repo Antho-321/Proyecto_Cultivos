3e6b58be380e5042d5ef9833087f0af271330acc
Antho-321 <allunav@utn.edu.ec>
2025-06-11 04:07:33 +0000

    cambio comprobacion gpu

--- comprobacion_gpu_amd.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# Lista dispositivos GPU reconocidos
-gpus = tf.config.list_physical_devices('GPU')
-print("GPUs reconocidas por DirectML:", gpus)
-
-for gpu in gpus:
-    print(f"  • {gpu.name} ({gpu.device_type})")
