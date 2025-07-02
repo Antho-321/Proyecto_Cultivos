d9539eb54897c7798f2fbd5a45258bfc042cd322
lizbethquilumba <jlquilumbad@utn.edu.ec>
2025-07-01 19:39:05 -0500

    actualizo model2

--- model2.py
+++ model2.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import timm
-from coordinate_attention import CoordAtt  # pip install coordinate-attention-pytorch
+from coordatt import CoordAtt  # pip install coordinate-attention-pytorch
 
 class ImprovedEfficientNetV2(nn.Module):
     """
